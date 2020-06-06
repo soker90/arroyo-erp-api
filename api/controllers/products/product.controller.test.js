969ad3c6cb5d5e4492723803abe759a282fe8932
Eduardo Parra <edue17@gmail.com>
2020-06-06 23:29:57 +0200

    new test

--- api/controllers/products/product.controller.test.js
+++ api/controllers/products/product.controller.test.js
@@ -1,7 +1,7 @@
 const supertest = require('supertest');
 const { mongoose } = require('arroyo-erp-models');
 const testDB = require('../../../test/test-db')(mongoose);
-// const requestLogin = require('../../../test/request-login');
+const requestLogin = require('../../../test/request-login');
 const app = require('../../..');
 
 
@@ -25,6 +25,36 @@ describe('ProductController', () => {
       test('Debería dar un 401', () => {
         expect(response.statusCode)
           .toBe(401);
+      });
+    });
+
+    describe('Usuario autenticado', () => {
+      let token;
+      before(done => {
+        requestLogin().then(res => {
+          token = res;
+          done();
+        });
+      });
+
+      describe('Crea un usuario correctamente', () => {
+        let response;
+
+        beforeAll(done => {
+          supertest(app)
+            .post('/products/ssss/price')
+            .set('Authorization', `Bearer ${token}`)
+            .send({ price: 11.2 })
+            .end((err, res) => {
+              response = res;
+              done();
+            });
+        });
+
+        test('Debería dar un 201', async () => {
+          expect(token).toBeTruthy();
+          expect(response.statusCode).toBe(201);
+        });
       });
     });
   });
