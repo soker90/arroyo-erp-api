b3d951fa7f231c667bd58746dad4e03869a03daf
Eduardo Parra <edue17@gmail.com>
2020-08-01 15:04:54 +0200

    fix

--- api/controllers/v0/invoices/invoices.controller.js
+++ api/controllers/v0/invoices/invoices.controller.js
@@ -100,8 +100,8 @@ class InvoicesController {
   invoiceConfirm(req, res) {
     logService.logInfo('[inovice]  - Confirm invoice');
     Promise.resolve(req)
-      // .tap(this.invoiceValidator.validateId)
-      // .tap(this.invoiceValidator.confirmParams)
+      .tap(this.invoiceValidator.validateId)
+      .tap(this.invoiceValidator.confirmParams)
       .then(this.invoiceService.invoiceConfirm)
       .tap(this.paymentService.create)
       .then(this.invoiceAdapter.dataResponse)
