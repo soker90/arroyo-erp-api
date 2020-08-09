3255b7c723e56b98a2b9bd70e6e9fc79dd981147
Eduardo Parra <edue17@gmail.com>
2020-08-09 14:13:49 +0200

    skip coverage

--- config/db.js
+++ config/db.js
@@ -15,13 +15,12 @@ const hostProperty = [].concat(host);
 const portProperty = [].concat(port);
 const hosts = hostProperty.reduce((s, h, i) => `${s}${i > 0 ? ',' : ''}${h}:${portProperty[i] || portProperty[0]}`, '');
 
-const uri = process.env.MONGODB ||`mongodb://${userPass}${hosts}/${dataBaseName}`;
+const uri = process.env.MONGODB || `mongodb://${userPass}${hosts}/${dataBaseName}`;
 
 const options = mongoOptions || {};
 options.useNewUrlParser = true;
 
 module.exports = () => {
-  if (process.env.NODE_ENV !== 'test') {
-    models.connect(uri, options);
-  }
+  /* istanbul ignore next */
+  if (process.env.NODE_ENV !== 'test') models.connect(uri, options);
 };
