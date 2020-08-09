02ecccc8c6d4f1703811a542d8b08c77c7d1b9eb
Eduardo Parra <edue17@gmail.com>
2020-08-09 14:20:54 +0200

    more tests

--- components/error-handlers/index.test.js
+++ components/error-handlers/index.test.js
@@ -65,4 +65,55 @@ describe('Error handlers', () => {
       expect(mockRes.payload.message).toBe('Unprocessable Entity');
     });
   });
+
+  describe('sendForbidden', () => {
+    const mockRes = _getMockResponse();
+
+    beforeAll(() => {
+      errorHandlers.sendForbidden(mockRes)(message);
+    });
+
+    test('It should contain an statusCode of 403', () => {
+      expect(mockRes.statusCode).toBe(403);
+    });
+
+    test('It should contain a payload with an error message', () => {
+      expect(mockRes.payload.error).toBe('Forbidden');
+      expect(mockRes.payload.message).toBe('Forbidden');
+    });
+  });
+
+  describe('sendConflict', () => {
+    const mockRes = _getMockResponse();
+
+    beforeAll(() => {
+      errorHandlers.sendConflict(mockRes)(message);
+    });
+
+    test('It should contain an statusCode of 409', () => {
+      expect(mockRes.statusCode).toBe(409);
+    });
+
+    test('It should contain a payload with an error message', () => {
+      expect(mockRes.payload.error).toBe('Conflict');
+      expect(mockRes.payload.message).toBe('Conflict');
+    });
+  });
+
+  describe('sendNotAcceptable', () => {
+    const mockRes = _getMockResponse();
+
+    beforeAll(() => {
+      errorHandlers.sendNotAcceptable(mockRes)(message);
+    });
+
+    test('It should contain an statusCode of 406', () => {
+      expect(mockRes.statusCode).toBe(406);
+    });
+
+    test('It should contain a payload with an error message', () => {
+      expect(mockRes.payload.error).toBe('Not Acceptable');
+      expect(mockRes.payload.message).toBe('Not Acceptable');
+    });
+  });
 });
