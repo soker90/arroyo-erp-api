95199b6533181e53b864fedd3e1f360c878954a0
Eduardo Parra <edue17@gmail.com>
2020-12-21 18:48:49 +0100

    added params search

--- api/services/invoice/services/invoices.js
+++ api/services/invoice/services/invoices.js
@@ -5,16 +5,30 @@ const { InvoiceModel } = require('arroyo-erp-models');
  * @param {Object} params
  * @returns {Promise<*>}
  */
-const invoices = ({ offset, limit, year }) => {
+const invoices = ({
+  offset,
+  limit,
+  year,
+  dateInvoice,
+  total,
+  nInvoice,
+}) => {
   const start = new Date(year);
   const nextYear = Number(year) + 1;
   const end = new Date(nextYear.toString());
+
+  const searchParams = {
+    ...(dateInvoice && { dateInvoice }),
+    ...(total && { total }),
+    ...(nInvoice && { nInvoice }),
+  };
 
   return InvoiceModel.find({
     dateRegister: {
       $gte: start,
       $lt: end,
     },
+    ...searchParams,
   }, '_id businessName nOrder dateInvoice total dateRegister dateInvoice nInvoice concept')
     .sort({ nOrder: -1 })
     .skip(offset || 0)
