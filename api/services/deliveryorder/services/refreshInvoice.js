47b8ec5e2d36f08b4d38dccbe6daff976863a5df
Eduardo Parra <edue17@gmail.com>
2021-01-19 19:23:55 +0100

    fix update do after remove invoice

--- api/services/deliveryorder/services/refreshInvoice.js
+++ api/services/deliveryorder/services/refreshInvoice.js
@@ -4,7 +4,7 @@ const { DeliveryOrderModel } = require('arroyo-erp-models');
  * Refresh delivery order and remove invoice assigned
  * @param {Object} invoice
  */
-const refreshInvoice = invoice => DeliveryOrderModel.updateOne(
+const refreshInvoice = invoice => DeliveryOrderModel.updateMany(
   { invoice: invoice._id },
   {
     $unset: {
