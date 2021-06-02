d1b54d4264f81fb32880fda2079e3924e18937d8
Eduardo Parra <edue17@gmail.com>
2021-06-03 00:21:11 +0200

    Added new templates for invoices of 2 and 3 pages

--- api/services/clientInvoice/services/export.js
+++ api/services/clientInvoice/services/export.js
@@ -1,13 +1,43 @@
+/* eslint-disable no-nested-ternary */
 const carbone = require('carbone');
-const { ClientInvoiceModel, ClientModel } = require('arroyo-erp-models');
+const {
+  ClientInvoiceModel,
+  ClientModel,
+} = require('arroyo-erp-models');
 const { formatDate } = require('../../../../utils');
+
+const INVOICE_1_TEMPLATE = './templates/factura-1-hoja.ods';
+const INVOICE_2_TEMPLATE = './templates/factura-2-hojas.ods';
+const INVOICE_3_TEMPLATE = './templates/factura-3-hojas.ods';
+
+const UNITS_WITHOUT_DECIMALS = ['ud', 'uds'];
+
+const generateWeight = ({
+  weight,
+  unit,
+}) => (UNITS_WITHOUT_DECIMALS.includes(unit)
+  ? `${weight} ${unit}`
+  : `${weight.toLocaleString('es-ES', {
+    minimumFractionDigits: 3,
+    maximumFractionDigits: 3,
+  })} ${unit}`);
 
 /* istanbul ignore next */
 const _productAdapter = product => ({
   descripcion: product.name,
-  peso: `${product.weight.toLocaleString('es-ES')} ${product.unit}`,
-  precio: `${product.price.toLocaleString('es-ES')} €`,
-  importe: `${product.total.toLocaleString('es-ES')} €`,
+  peso: generateWeight(product),
+  precio: product.price.toLocaleString('es-ES', {
+    style: 'currency',
+    currency: 'EUR',
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+  }),
+  importe: product.total.toLocaleString('es-ES', {
+    style: 'currency',
+    currency: 'EUR',
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+  }),
 });
 
 /* istanbul ignore next */
@@ -24,26 +54,42 @@ const _doAdapter = deliveryOrder => {
 };
 
 /* istanbul ignore next */
-const _invoicesAdapter = invoice => ({
-  fecha: formatDate(invoice.date),
-  nFactura: invoice.nInvoice,
-  empresa: invoice.client.businessName,
-  direccion: invoice.client.address,
-  ciudad: invoice.client.city,
-  cp: invoice.client.postalCode,
-  provincia: invoice.client.province,
-  cif: invoice.client.cif,
-  filas: invoice.deliveryOrders.map(_doAdapter).flat(),
-  base: invoice.taxBase,
-  iva: invoice.iva,
-  total: invoice.total,
-});
+const _invoicesAdapter = invoice => {
+  const filasAll = invoice.deliveryOrders.map(_doAdapter)
+    .flat();
+
+  return ({
+    fecha: formatDate(invoice.date),
+    nFactura: invoice.nInvoice,
+    empresa: invoice.client.businessName,
+    direccion: invoice.client.address,
+    ciudad: invoice.client.city,
+    cp: invoice.client.postalCode,
+    provincia: invoice.client.province,
+    cif: invoice.client.cif,
+    filas: filasAll.slice(0, 49),
+    filas2: filasAll.slice(50, 99),
+    filas3: filasAll.slice(100, 200),
+    nFilas: filasAll.length,
+    base: invoice.taxBase,
+    iva: invoice.iva,
+    total: invoice.total,
+  });
+};
 
 /* istanbul ignore next */
 const _getInvoice = async id => {
-  const invoice = await ClientInvoiceModel.findOne({ _id: id }).populate('client', null, ClientModel);
+  const invoice = await ClientInvoiceModel.findOne({ _id: id })
+    .populate('client', null, ClientModel);
 
   return _invoicesAdapter(invoice);
+};
+
+/* istanbul ignore next */
+const _getTemplate = ({ nFilas }) => {
+  if (nFilas < 51) return INVOICE_1_TEMPLATE;
+  if (nFilas < 101) return INVOICE_2_TEMPLATE;
+  return INVOICE_3_TEMPLATE;
 };
 
 /* istanbul ignore next */
@@ -53,7 +99,9 @@ const exportOds = async ({ id }) => {
   let bookFile = null;
   let error = null;
 
-  carbone.render('./templates/invoice-client.ods', invoice, {
+  const template = _getTemplate(invoice);
+
+  carbone.render(template, invoice, {
     lang: 'es-es',
   }, (err, result) => {
     /* istanbul ignore next */
